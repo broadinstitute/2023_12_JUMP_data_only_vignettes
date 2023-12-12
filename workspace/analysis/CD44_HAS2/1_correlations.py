50313053fe384ac58c24eb275250cee6b45e82e4
Alán F. Muñoz <amunozgo@broadinstitute.org>
2023-12-12 15:40:52 -0500

    change(CD44): Homogeneise processing notebook

--- workspace/analysis/CD44_HAS2/1_correlations.py
+++ workspace/analysis/CD44_HAS2/1_correlations.py
@@ -17,53 +17,72 @@
 Find correlations between CD44 and HAS2. Question inspired by Prof. Chonghui Chen from Baylor College of Medicine during ASCB2 2023.
 """
 
+from itertools import product
+from pathlib import Path
+
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 import seaborn as sns
 from broad_babel.query import run_query
-from utils import load_path, get_figs_dir
+from copairs.compute import pairwise_cosine
+
+from utils import get_figs_dir, load_path
 
 figs_dir = get_figs_dir()
 df = load_path("../../profiles/harmonized_no_sphering_profiles.parquet")
 
 genes_of_interest = ("CD44", "HAS2")
-cd44_crispr, *cd44_orf = run_query(
-    query="CD44", input_column="standard_key", output_column="JCP2022"
-)[0]
+cd44_crispr = run_query(query="CD44", input_column="standard_key", output_column="*")
 
 gene_matches = {pert: dict() for pert in genes_of_interest}
 for gene in genes_of_interest:
-    crispr, *orf = list(
+    results = list(
         map(
-            lambda x: x[0],
-            run_query(query=gene, input_column="standard_key", output_column="JCP2022"),
+            lambda x: x,
+            run_query(query=gene, input_column="standard_key", output_column="*"),
         )
     )
-    gene_matches[gene]["crispr"] = crispr
-    if len(orf):
-        gene_matches[gene]["orf"] = orf[0]
-
+    for pert_type in ("crispr", "orf"):
+        result_array = [x[1] for x in results if x[0] == pert_type]
+        if len(result_array):
+            gene_matches[gene][pert_type] = result_array[0]
 
 # %% Calculate the correlations between has2 and cd44 based on CRISPR data
 
+
 feat_cols = [x for x in df.columns if not x.startswith("Metadata")]
-data_only = df.loc[
-    df["Metadata_JCP2022"].isin([x["crispr"] for x in gene_matches.values()])
-]
-data_only.set_index(
-    data_only["Metadata_JCP2022"].map(
-        {v["crispr"]: k for k, v in gene_matches.items()}
-    ),
+translator_crispr_jump_gene = {v.get("crispr"): k for k, v in gene_matches.items()}
+subset = df.loc[df["Metadata_JCP2022"].isin(translator_crispr_jump_gene)]
+subset.set_index(
+    subset["Metadata_JCP2022"].map(translator_crispr_jump_gene),
     inplace=True,
 )
-for method in ("pearson", "kendall", "spearman"):
-    candidates_corr = (
-        data_only[feat_cols].T.corr(method=method).sort_index().sort_index(axis=1)
+
+data_subset_no_features = subset[feat_cols]
+n_samples = len(subset)
+all_pair_ixs = np.array(list(product(range(n_samples), range(n_samples))))
+cosine_distance = pairwise_cosine(
+    data_subset_no_features.values, all_pair_ixs, batch_size=min(200, n_samples)
+).reshape((n_samples, n_samples))
+cosine_df = (
+    pd.DataFrame(
+        data=cosine_distance,
+        index=subset.index,
+        columns=subset.index,
     )
-    sns.heatmap(candidates_corr, robust=True)
-    plt.tight_layout()
-    plt.savefig(
-        figs_dir
-        / f"correlations_{method}_harmony_{'_'.join(genes_of_interest).lower() }.png",
-        dpi=300,
-    )
-    plt.close()
+    .sort_index(axis=0)
+    .sort_index(axis=1)
+)
+sns.heatmap(
+    cosine_df,
+    robust=True,
+)
+plt.tight_layout()
+plt.savefig(
+    figs_dir / f"heatmap_cosdist_harmony_{'_'.join(genes_of_interest).lower() }.png",
+    dpi=300,
+)
+plt.close()
+
+# %% TODO Once acquired, use ORF data to have a look at these profiles.

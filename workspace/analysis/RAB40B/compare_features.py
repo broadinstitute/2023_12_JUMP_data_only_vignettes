f60da9b1696b55a8ad707aec42bd55414a52a733
Alán F. Muñoz <amunozgo@broadinstitute.org>
2024-01-29 09:33:55 -0500

    feat(RAB40B): Incorporate feature Mann-Whitney

--- workspace/analysis/RAB40B/compare_features.py
+++ workspace/analysis/RAB40B/compare_features.py
@@ -11,7 +11,10 @@
 
 from pathlib import Path
 
+import pandas as pd  # TODO stick to polars
 import polars as pl
+import scipy.stats as stats
+from statsmodels.stats.multitest import multipletests
 
 fpath = Path(
     "/dgx1nas1/storage/data/shared/morphmap_profiles/orf/full_profiles_cc_adj_mean_corr.parquet"
@@ -35,3 +38,36 @@ medians = clusters.group_by("Metadata_Cluster").median()
 diff = medians.select(pl.all().exclude("^Metadata.*$").diff())
 feature_diff = diff.filter(~pl.all_horizontal(pl.all().is_null())).melt()
 feature_diff.sort(by="value").write_csv("feature_diffs.csv")
+
+
+cluster_a = clusters.filter(pl.col("Metadata_Cluster") == "RAB40B/C").select(
+    pl.all().exclude("^Metadata.*$")
+)
+cluster_b = clusters.filter(pl.col("Metadata_Cluster") == "INS/PIK").select(
+    pl.all().exclude("^Metadata.*$")
+)
+results = []
+for feature in cluster_a.columns:
+    cluster_A = cluster_a.get_column(feature)
+    cluster_B = cluster_b.get_column(feature)
+
+    # Use t-test or Mann-Whitney U test based on data distribution
+    t_stat, p_val = stats.ttest_ind(cluster_A, cluster_B, equal_var=False)
+
+    results.append({"Feature": feature, "T-Statistic": t_stat, "P-Value": p_val})
+
+
+results_df = pd.DataFrame(results)
+
+# drop rows with NaN
+results_df = results_df.dropna()
+
+# Apply multiple testing correction
+
+# corrected_pvals = multipletests(results_df["P-Value"], method="fdr_bh")[1]
+corrected_pvals = multipletests(results_df["P-Value"], method="fdr_bh")[1]
+results_df["Corrected P-Value"] = corrected_pvals
+
+# Filter significant features
+significant_features = results_df[results_df["Corrected P-Value"] < 0.05]
+significant_features.to_csv("significant_features.csv")

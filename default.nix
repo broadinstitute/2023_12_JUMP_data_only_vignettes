41d081df9bb6eefb86b0c2cadc8221bacd49ad4a
Alán F. Muñoz <amunozgo@broadinstitute.org>
2024-03-20 20:40:43 -0400

    fix(nix): remove space

--- default.nix
+++ default.nix
@@ -5,7 +5,7 @@ let
 
   poetry2nix-src = fetchFromGitHub {
       owner = "nix-community";
-      repo = "poetr y2nix";
+      repo = "poetry2nix";
       rev = "7df29134065172f24385177ea69e755cb90f196c";
       sha256 = "0zz3qzp2b5i9gw4yfxfrq07iadcdadackph12h02w19bb3535rm6";
   };

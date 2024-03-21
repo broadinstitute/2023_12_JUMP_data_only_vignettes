9c626c022efad40129af630bffc9269e5c6b0711
Alán F. Muñoz <amunozgo@broadinstitute.org>
2024-03-20 22:12:45 -0400

    change(nix): use nix-shell to add quarto

--- default.nix
+++ default.nix
@@ -1,5 +1,4 @@
 { pkgs ? import <nixpkgs> {} }:
-
 let
   inherit (pkgs) fetchFromGitHub callPackage;
 
@@ -9,12 +8,12 @@ let
       rev = "3c92540";
       sha256 = "sha256-2GOiFTkvs5MtVF65sC78KNVxQSmsxtk0WmV1wJ9V2ck=";
   };
-
   poetry2nix = callPackage poetry2nix-src { };
-
-in
-  poetry2nix.mkPoetryEnv {
+  myAppEnv = poetry2nix.mkPoetryEnv {
     projectDir = ./.;
     preferWheels = true; # TODO use overrides to fix this
     # It has to do with the maturin rust dependencies
-}
+  };
+in myAppEnv.env.overrideAttrs (oldAttrs: {
+  buildInputs = [ pkgs.quarto ];
+})

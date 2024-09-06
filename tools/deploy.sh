8966e905b3def45ce78f4e80ad0132dcdab699aa
Alan Munoz <afer.mg@gmail.com>
2024-09-06 14:55:58 -0400

    tools: add echo to deploy.sh

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -4,6 +4,7 @@ git config --global user.email quarto-github-actions-publish@example.com &&
 git config --global user.name  Quarto_GHA_Runner &&
 export QUARTO_PYTHON=$(which python) &&
 quarto publish gh-pages &&
+echo "Generating collab files" &&
 git restore --source gh-pages -- "howto/*.ipynb" &&
 python tools/insert_colab_cell.py &&
 git add colab && git commit -m 'add colab files' && git push --force origin main:colab

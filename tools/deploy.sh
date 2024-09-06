19517848aeb1e051536c8d87acdcb6b933a35b96
Alan Munoz <afer.mg@gmail.com>
2024-09-06 15:05:30 -0400

    test: add checks

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,11 +1,15 @@
 #/usr/bin/env bash
+# Script that converts python scripts into notebooks, runs and publishes them to a website using Quarto and
+# generates a notebook in another branch that mimics the original one, but adds a cell containing '!pip install jump_deps'
 jupytext --sync scripts/*.py &&
 git config --global user.email quarto-github-actions-publish@example.com &&
 git config --global user.name  Quarto_GHA_Runner &&
 export QUARTO_PYTHON=$(which python) &&
 quarto publish gh-pages &&
 echo "Generating collab files" &&
+git status &&
 git restore --source gh-pages -- "howto/*.ipynb" &&
+ls howto &&
 python tools/insert_colab_cell.py &&
 ls colab &&
 git add colab/* && git commit -m 'add colab files' && git push --force origin main:colab

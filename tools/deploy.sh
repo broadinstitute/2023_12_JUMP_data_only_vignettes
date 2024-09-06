107457f1d541bbb1f0e8dea8c065de327456b8f5
Alan Munoz <afer.mg@gmail.com>
2024-09-06 14:49:09 -0400

    feat: add deploy.sh and integrate intoa ctions

---
+++ tools/deploy.sh
@@ -0,0 +1,9 @@
+#/usr/bin/env bash
+jupytext --sync scripts/*.py &&
+git config --global user.email quarto-github-actions-publish@example.com &&
+git config --global user.name  Quarto_GHA_Runner &&
+export QUARTO_PYTHON=$(which python) &&
+quarto publish gh-pages &&
+git restore --source gh-pages -- "howto/*.ipynb" &&
+python tools/insert_colab_cell.py &&
+git add colab && git commit -m 'add colab files' && git push --force origin main:colab
